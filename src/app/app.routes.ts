2de34f15acdf45a6b6868dfdfe0a34a88b705f95
Shoonkey <shoonkey.dev@gmail.com>
2025-04-19 23:01:42 -0300

    chore: added new placeholder route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,10 @@
 import { Component } from "@angular/core";
 import { Routes } from "@angular/router";
+import { ExamplePageComponent } from "./pages/example-page/example-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "example",
+    component: ExamplePageComponent
+  }
+];
